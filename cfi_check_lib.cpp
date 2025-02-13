4cd4373a41d56dbb6e01a3ce028a055e5c693010
emilybrouillet <89803301+emilybrouillet@users.noreply.github.com>
2025-02-13 01:34:19 -0600

    Update cfi_check_lib.cpp

--- cfi_check_lib.cpp
+++ cfi_check_lib.cpp
@@ -2,13 +2,10 @@
 #include <unordered_map>
 #include <unordered_set>
 
-// Map from basic block IDs to allowed target IDs
 std::unordered_map<int, std::unordered_set<int>> ValidTransitions;
 
-// Global variable to track the current basic block ID
 int currentBBID = -1;
 
-// Function to add valid transitions
 extern "C" void initCFG() {
 
     // This is hardcoded for simplicity
@@ -19,24 +16,16 @@ extern "C" void initCFG() {
     ValidTransitions[4].insert(-1); 
     ValidTransitions[5].insert(-1);
 
-    /* Insert more valid targets */
-    /* . . . */
 }
 
 // Runtime CFI check function
 extern "C" void __cfi_check_bb(int targetBBID) {
-    /* iterate the data structure `ValidTransitions` */
-    /* check if <CurrentBBID, targetBBID> is in `ValidTransitions`  */
     
     if (ValidTransitions[currentBBID].count(targetBBID)) { 
-        // Allowed transition, update current basic block ID currentBBID = targetBBID; } 
-        // Update the current basic block ID
         extern "C" void __cfi_check_bb(int targetBBID) {
     if (ValidTransitions[currentBBID].count(targetBBID)) {
-        // Allowed transition, update current basic block ID
         currentBBID = targetBBID;
     } else {
-        // Check if the current block is terminal
         if (ValidTransitions[currentBBID].count(-1)) {
             printf("CFI Violation: No valid transitions allowed from BB-%d!\n", currentBBID);
             exit(1);
